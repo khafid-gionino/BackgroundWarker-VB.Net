04bfca8934c007153ca337d13a71998482f498b9
khafid-gionino <achmadkhafids75@gmail.com>
2020-03-31 11:57:17 +0700

    commit

---
+++ backgroundwarker/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(43, 53)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(75, 23)
+        Me.Start.TabIndex = 0
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'Cancel
+        '
+        Me.Cancel.Location = New System.Drawing.Point(151, 53)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.Cancel.TabIndex = 1
+        Me.Cancel.Text = "Cancel"
+        Me.Cancel.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(43, 104)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(261, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(327, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 154)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Viner Hand ITC", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(126, 226)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(152, 26)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Ach.Khafid Salim"
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.Start)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Start As Button
+    Friend WithEvents Cancel As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ backgroundwarker/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'setting property backgroundworker
+        BackgroundWorker1.WorkerSupportsCancellation = True 'agar backgroundworker bisa di cancel
+        BackgroundWorker1.WorkerReportsProgress = True 'agar backgroundworker dapat memperbarui report progress
+    End Sub
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+        ' jika backgroundworker tidak sedang berjalan/busy
+        If (BackgroundWorker1.IsBusy = False) Then
+            ' menjalankan backgroundworker
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub Cancel_Click(sender As Object, e As EventArgs) Handles Cancel.Click
+        'jika backgroundworker sedang berjalan
+        If (BackgroundWorker1.IsBusy) Then
+            'membatalkan proses backgroundworker
+            BackgroundWorker1.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        ' perulangan untuk mengisi value pada progressbar
+        For i As Integer = 0 To 100
+
+            Dim status As String = ""
+            If (i >= 1 And i < 30) Then
+                status = "Inisialisasi"
+            ElseIf (i >= 30 And i < 70) Then
+                status = "Pengecekan file configurasi"
+
+            ElseIf (i >= 70 And i < 100) Then
+                status = "Pengecekan Database"
+            ElseIf (i = 100) Then
+                status = "Selesai"
+            End If
+
+            ' memperbarui reportprogress
+            BackgroundWorker1.ReportProgress(i, status)
+
+            System.Threading.Thread.Sleep(100)
+
+            ' jika backgroundWorker1 di batalkan
+            If (BackgroundWorker1.CancellationPending) Then
+                e.Cancel = True
+                Exit Sub
+            End If
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = Convert.ToDouble(e.ProgressPercentage) 'memperbarui value pada progressbar
+        Label1.Text = e.ProgressPercentage.ToString() & "%" ' memperbarui text pada label1
+        Label2.Text = DirectCast(e.UserState, String) ' memberparui text pada label2
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If (e.Cancelled = True) Then 'jika backgroundWorker1 dibatalkan
+            MessageBox.Show("Proses dibatalkan")
+        ElseIf (e.Error IsNot Nothing) Then ' jika terjadi error selama backgroundworker berjalan
+            MessageBox.Show(e.Error.Message)
+        Else
+            MessageBox.Show("Proses selesai")
+        End If
+    End Sub
+End Class
